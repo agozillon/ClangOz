1b42047cfa4c540791d0b790fd76389c895dbd08
Paul Keir <pkeir@outlook.com>
2023-05-24 18:32:28 +0100

    Data transposition can allow a single benchmark pass.

--- par-constexpr-tests/benchmark/doit.sh
+++ par-constexpr-tests/benchmark/doit.sh
@@ -7,8 +7,8 @@ function runprog {
   local num_core_sizes=$4
   local -n sizes=$5  # array by name reference https://stackoverflow.com/a/26443029
   local num_sizes=$6
-  local i j k delta total avg
-  local CFLAGS="-O3 -std=c++20 -stdlib=libc++ -DCONSTEXPR_TRACK_TIME -DCONSTEXPR_SYCL -fconstexpr-steps=4294967295 --no-warnings -I$CEST_INCLUDE -I$MOTORSYCL_INCLUDE"
+  local i j k delta total avg serial
+  local CFLAGS="-O3 -std=c++20 -stdlib=libc++ -DCONSTEXPR_TRACK_TIME -fconstexpr-steps=4294967295 --no-warnings -I$CEST_INCLUDE"
   local PARFLAGS SZFLAG
 
   # echo $@
@@ -19,13 +19,20 @@ function runprog {
     SZFLAG="-DSZ=${sizes[$j]}"
 
     for (( k = 0; k < nruns; k++ )); do
-      delta="$($CLANGOZ_ROOT/bin/clang++ ${CFLAGS}               ${SZFLAG} ../../sycl/${basename}.cpp)"
+      delta="$($CLANGOZ_ROOT/bin/clang++ ${CFLAGS}               ${SZFLAG} ../../${basename}.cpp)"
       total=$(echo $total + $delta | bc)   # using bc for floating point
     done
 
     avg=$(echo "scale=5; $total / $nruns" | bc -l)  # 5 is the fp precision
+    serial+=($avg) # used as the base line for calculating speedup below
     echo $avg ${sizes[$j]} | tee -a ${basename}.dat
 
+  done
+
+  # here is the output for the ideal speedup line (with (0,0) added at the start)
+  echo 0 0
+  for (( i = 0; i < num_core_sizes; i++ )); do
+    echo ${cores[$i]} ${cores[$i]}
   done
 
   for (( i = 0; i < num_core_sizes; i++ )); do
@@ -37,27 +44,27 @@ function runprog {
       SZFLAG="-DSZ=${sizes[$j]}"
 
       for (( k = 0; k < nruns; k++ )); do
-#        echo $CLANGOZ_ROOT/bin/clang++ ${CFLAGS} ${PARFLAGS} ${SZFLAG} ../../${basename}.cpp
-        delta="$($CLANGOZ_ROOT/bin/clang++ ${CFLAGS} ${PARFLAGS} ${SZFLAG} ../../sycl/${basename}.cpp)"
+        delta="$($CLANGOZ_ROOT/bin/clang++ ${CFLAGS} ${PARFLAGS} ${SZFLAG} ../../${basename}.cpp)"
         total=$(echo $total + $delta | bc)   # using bc for floating point
       done
 
       avg=$(echo "scale=5; $total / $nruns" | bc -l)  # 5 is the fp precision
       echo $avg ${sizes[$j]} | tee -a ${basename}.dat
+      echo OK ${cores[$i]} $(echo "scale=5; ${serial[$j]} / $avg" | bc -l)
 
     done
   done
 }
 
-NUM_RUNS=3 # For averaging, stddev etc.
+NUM_RUNS=2 # For averaging, stddev etc.
 CORE_COUNTS=(2 4 6 8 16)  # 16: Hyper-threading: 2 threads per core on i9-12900K (8 cores)
 NUM_CORE_SIZES=2 # 5 # Could be made less than the size of CORE_COUNTS
 
 MAND_SIZES=(32 64 128 256 512)
-NUM_MAND_SIZES=3
+NUM_MAND_SIZES=2
 
 BS_SIZES=(4 16 1024 4096 16384 65536)
-NUM_BS_SIZES=3
+NUM_BS_SIZES=2
 
 NBODY_SIZES=(16 32 64 128 256)
 NUM_NBODY_SIZES=2
@@ -66,8 +73,7 @@ TMPDIR=$(mktemp -d --tmpdir=mytemp)
 cd $TMPDIR
 
 #runprog cexpr_mandelbrot   $NUM_RUNS CORE_COUNTS $NUM_CORE_SIZES MAND_SIZES  $NUM_MAND_SIZES
-#runprog cexpr_blackscholes $NUM_RUNS CORE_COUNTS $NUM_CORE_SIZES BS_SIZES    $NUM_BS_SIZES
-#runprog cexpr_nbody        $NUM_RUNS CORE_COUNTS $NUM_CORE_SIZES NBODY_SIZES $NUM_BS_SIZES
-runprog cexpr_sycl_edge_detection $NUM_RUNS CORE_COUNTS $NUM_CORE_SIZES NBODY_SIZES $NUM_BS_SIZES
+runprog cexpr_blackscholes $NUM_RUNS CORE_COUNTS $NUM_CORE_SIZES BS_SIZES    $NUM_BS_SIZES
+runprog cexpr_nbody        $NUM_RUNS CORE_COUNTS $NUM_CORE_SIZES NBODY_SIZES $NUM_BS_SIZES
 
 cd - > /dev/null
