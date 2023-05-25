cb3f8223557918a25e7d3405889934d9fd5aaa96
Paul Keir <pkeir@outlook.com>
2023-05-25 15:38:49 +0100

    Finalised benchmark script.

--- par-constexpr-tests/benchmark/doit.sh
+++ par-constexpr-tests/benchmark/doit.sh
@@ -7,11 +7,12 @@ function runprog {
   local num_core_sizes=$4
   local -n sizes=$5  # array by name reference https://stackoverflow.com/a/26443029
   local num_sizes=$6
-  local i j k delta total avg serial
+  local i j k delta total avg serial speedup
   local CFLAGS="-O3 -std=c++20 -stdlib=libc++ -DCONSTEXPR_TRACK_TIME -fconstexpr-steps=4294967295 --no-warnings -I$CEST_INCLUDE"
   local PARFLAGS SZFLAG
+  local timings_out_file=${basename}.dat speedup_out_file=${basename}_speedup.dat
 
-  # echo $@
+  echo \# $@
 
   echo "# Serial" | tee -a ${basename}.dat    # tee -a: append, do not overwrite
   for (( j = 0; j < num_sizes; j++ )); do
@@ -25,19 +26,20 @@ function runprog {
 
     avg=$(echo "scale=5; $total / $nruns" | bc -l)  # 5 is the fp precision
     serial+=($avg) # used as the base line for calculating speedup below
-    echo $avg ${sizes[$j]} | tee -a ${basename}.dat
+    echo $avg ${sizes[$j]} | tee -a ${timings_out_file}
 
   done
 
-  # here is the output for the ideal speedup line (with (0,0) added at the start)
-  echo 0 0
+  # output for ideal speedup line ((0,0) is origin - the first 0 just for sorting)
+  echo 0 0 0 >> ${speedup_out_file}
   for (( i = 0; i < num_core_sizes; i++ )); do
-    echo ${cores[$i]} ${cores[$i]}
+    #echo 0 ${cores[$i]} ${cores[$i]} | tee -a ${speedup_out_file}
+    echo 0 ${cores[$i]} ${cores[$i]} >> ${speedup_out_file}
   done
 
   for (( i = 0; i < num_core_sizes; i++ )); do
     PARFLAGS="-DCONSTEXPR_PARALLEL -fexperimental-constexpr-parallel -fconstexpr-parallel-partition-size=${cores[$i]}"
-    echo "#" ${cores[$i]} "Threads" | tee -a ${basename}.dat
+    echo "#" ${cores[$i]} "Threads" | tee -a ${timings_out_file}
 
     for (( j = 0; j < num_sizes; j++ )); do
       total=0
@@ -49,22 +51,27 @@ function runprog {
       done
 
       avg=$(echo "scale=5; $total / $nruns" | bc -l)  # 5 is the fp precision
-      echo $avg ${sizes[$j]} | tee -a ${basename}.dat
-      echo OK ${cores[$i]} $(echo "scale=5; ${serial[$j]} / $avg" | bc -l)
+      speedup=$(echo "scale=5; ${serial[$j]} / $avg" | bc -l)
+      echo -n "${speedup} "
+      echo $avg ${sizes[$j]} | tee -a ${timings_out_file}
+      echo ${sizes[$j]} ${cores[$i]} ${speedup} >> ${speedup_out_file}
 
     done
   done
+
+  # Sort by col 1 (datasize) and remove it. sponge is in APT package moreutils
+  cat ${basename}_speedup.dat | sort -n | cut -d ' ' -f2- | sponge ${speedup_out_file}
 }
 
 NUM_RUNS=2 # For averaging, stddev etc.
 CORE_COUNTS=(2 4 6 8 16)  # 16: Hyper-threading: 2 threads per core on i9-12900K (8 cores)
-NUM_CORE_SIZES=2 # 5 # Could be made less than the size of CORE_COUNTS
+NUM_CORE_SIZES=4 # 5 # Could be made less than the size of CORE_COUNTS
 
 MAND_SIZES=(32 64 128 256 512)
 NUM_MAND_SIZES=2
 
 BS_SIZES=(4 16 1024 4096 16384 65536)
-NUM_BS_SIZES=2
+NUM_BS_SIZES=3
 
 NBODY_SIZES=(16 32 64 128 256)
 NUM_NBODY_SIZES=2
@@ -72,8 +79,11 @@ NUM_NBODY_SIZES=2
 TMPDIR=$(mktemp -d --tmpdir=mytemp)
 cd $TMPDIR
 
+echo "# (Non-serial) Columns represent speedup time and datasize"
 #runprog cexpr_mandelbrot   $NUM_RUNS CORE_COUNTS $NUM_CORE_SIZES MAND_SIZES  $NUM_MAND_SIZES
 runprog cexpr_blackscholes $NUM_RUNS CORE_COUNTS $NUM_CORE_SIZES BS_SIZES    $NUM_BS_SIZES
-runprog cexpr_nbody        $NUM_RUNS CORE_COUNTS $NUM_CORE_SIZES NBODY_SIZES $NUM_BS_SIZES
+#runprog cexpr_nbody        $NUM_RUNS CORE_COUNTS $NUM_CORE_SIZES NBODY_SIZES $NUM_BS_SIZES
 
 cd - > /dev/null
+
+echo Output was stored in $TMPDIR
